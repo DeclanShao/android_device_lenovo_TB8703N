a05da3728784f735a0ba4f057579d22249b79b8c
Shukla Mradul <mradul_shukla@codeaurora.org>
2016-05-21 11:02:00 +0200

    tb8703n: QCamera2:HAL : HAL1 Removal during Compile time
    Issue:
    Compile time HAL1 Removal as for IOT projects we are going to use only HAL3 so
    removing HAL1 Camera files from compile time.
    
    fix:
    Modified the Android.mk file to remove HAL1 during compilation.In
    case if HAL1 is not getting compiled muxer object is also not created
    in QCamera2Factory.
    
    Change-Id: I488dccc0e1fd13ffadea8cbcfe54caa35a29ac40
    CRs-Fixed: 1021955

--- camera/QCamera2/HAL3/QCamera3Channel.cpp
+++ camera/QCamera2/HAL3/QCamera3Channel.cpp
@@ -36,7 +36,9 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "gralloc.h"
+#include "hardware/gralloc.h"
+#include <utils/Timers.h>
+#include <sys/stat.h>
 
 // Camera dependencies
 #include "QCamera3Channel.h"
